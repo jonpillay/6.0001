607b6a33b5d6735bfb16011627fc8acffd684efe
Jon <jon.pillay@gmail.com>
2022-06-23 23:49:08 +0100

    After a brief hiatus for some hospitality work, and some fun with python datatime objects, I have completed Problem Set 5 which is an RSS feed. However even though test.py program return OK the feed doesn’t update. It looks like one of the packages is out of date (problem is 6 years old at this point) as throws up the error Polling . . . module 'collections' has no attribute 'Callable'. A quick Google search suggests importing ‘collections’ and assigning via that, but has no affect. I’m too eager to get on with the next course (Computational Thinking and Data Science) to fix the problem so will leave it for now.

--- ps5/ps5.py
+++ ps5/ps5.py
@@ -3,13 +3,18 @@
 # Collaborators:
 # Time:
 
+from dataclasses import replace
+from locale import normalize
+from optparse import TitledHelpFormatter
+from types import NoneType
 import feedparser
 import string
 import time
 import threading
 from project_util import translate_html
 from mtTkinter import *
-from datetime import datetime
+from datetime import datetime, tzinfo, timedelta, timezone
+from pytz import timezone
 import pytz
 
 
@@ -54,7 +59,28 @@ def process(url):
 
 # Problem 1
 
-# TODO: NewsStory
+class NewsStory(object):
+    def __init__(self, guid, title, description, link, pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
+
+    def get_guid(self):
+        return self.guid
+
+    def get_title(self):
+        return self.title
+
+    def get_description(self):
+        return self.description
+
+    def get_link(self):
+        return self.link
+
+    def get_pubdate(self):
+        return self.pubdate
 
 
 #======================
@@ -75,16 +101,68 @@ class Trigger(object):
 # Problem 2
 # TODO: PhraseTrigger
 
-# Problem 3
-# TODO: TitleTrigger
+class PhraseTrigger(Trigger):
+
+    def is_phrase_in(self):
+        raise NotImplementedError
+
+    def evaluate(self):
+        raise NotImplementedError
+
+class TitleTrigger(PhraseTrigger):
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+
+    def is_phrase_in(self, text, phrase):
+        clean_title = "".join([(n.lower() if n not in string.punctuation else " ") for n in text])
+        use_title_list = clean_title.split()
+        clean_phrase = "".join([(n.lower() if n not in string.punctuation else " ") for n in phrase])
+        use_phrase_list = clean_phrase.split()
+        for i in range(len(use_title_list)-(len(use_phrase_list)-1)):
+            if use_phrase_list[0] == use_title_list[i]:
+                for m in range(len(use_phrase_list)):
+                    if use_phrase_list[m] == use_title_list[m+i]:
+                        if m == len(use_phrase_list)-1:
+                            return True
+    
+    def evaluate(self, story):
+        if self.is_phrase_in(story.get_title(), self.phrase) == True:
+            return True
+
 
 # Problem 4
-# TODO: DescriptionTrigger
+#DescriptionTrigger
+
+class DescriptionTrigger(PhraseTrigger):
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+
+    def is_phrase_in(self, text, phrase):
+        clean_title = "".join([(n.lower() if n not in string.punctuation else " ") for n in text])
+        use_title_list = clean_title.split()
+        clean_phrase = "".join([(n.lower() if n not in string.punctuation else " ") for n in phrase])
+        use_phrase_list = clean_phrase.split()
+        for i in range(len(use_title_list)-(len(use_phrase_list)-1)):
+            if use_phrase_list[0] == use_title_list[i]:
+                for m in range(len(use_phrase_list)):
+                    if use_phrase_list[m] == use_title_list[m+i]:
+                        if m == len(use_phrase_list)-1:
+                            return True
+
+    def evaluate(self, story):
+        if self.is_phrase_in(story.get_description(), self.phrase) == True:
+            return True
 
 # TIME TRIGGERS
 
 # Problem 5
-# TODO: TimeTrigger
+
+class TimeTrigger(Trigger):
+    def time_check(self):
+        raise NotImplementedError
+
+    def evaluate(self):
+        raise NotImplementedError
 # Constructor:
 #        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
 #        Convert time from string to a datetime before saving it as an attribute.
@@ -92,17 +170,79 @@ class Trigger(object):
 # Problem 6
 # TODO: BeforeTrigger and AfterTrigger
 
+class BeforeTrigger(TimeTrigger):
+    def __init__(self, moment):
+        tz_moment = moment
+        self.moment = datetime.strptime(tz_moment, '%d %b %Y %H:%M:%S')
+            
+    def time_check(self, pubdate):
+        try:
+            if pubdate < self.moment:
+                return True
+        except:
+            #self.moment = self.moment.astimezone(pytz.timezone("EST"))
+            self.moment = self.moment.replace(tzinfo=pytz.timezone('EST'))
+            if pubdate < self.moment:
+                return True
+
+    def evaluate(self, story):
+        if self.time_check(story.get_pubdate()):
+            return True
+
+class AfterTrigger(TimeTrigger):
+    def __init__(self, moment):
+        tz_moment =  moment
+        self.moment = datetime.strptime(tz_moment, '%d %b %Y %H:%M:%S')
+
+    def time_check(self, pubdate):
+        try:
+            if pubdate > self.moment:
+                return True
+        except:
+            #self.moment = self.moment.astimezone(pytz.timezone("EST"))
+            self.moment =self.moment.replace(tzinfo=pytz.timezone('EST'))
+            if pubdate > self.moment:
+                return True
+
+    def evaluate(self, story):
+        if self.time_check(story.get_pubdate()) == True:
+            return True
+
 
 # COMPOSITE TRIGGERS
 
 # Problem 7
 # TODO: NotTrigger
 
+class NotTrigger(Trigger):
+    def __init__(self, trigger):
+        self.trigger = trigger
+    
+    def evaluate(self, story):
+        if self.trigger.evaluate(story) != True:
+            return True
+
+
 # Problem 8
 # TODO: AndTrigger
 
+class AndTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+
+    def evaluate(self, story):
+        if self.trigger1.evaluate(story) and self.trigger2.evaluate(story) == True:
+            return True
+
 # Problem 9
 # TODO: OrTrigger
+
+class OrTrigger(AndTrigger):
+
+    def evaluate(self, story):
+        if self.trigger1.evaluate(story) or self.trigger2.evaluate(story) == True:
+            return True
 
 
 #======================
@@ -119,7 +259,20 @@ def filter_stories(stories, triggerlist):
     # TODO: Problem 10
     # This is a placeholder
     # (we're just returning all the stories, with no filtering)
-    return stories
+
+    triggered_stories = []
+
+    for i in range(len(stories)):
+        for j in triggerlist:
+            if j.evaluate(stories[i]) == True:
+                triggered_stories.append(stories[i])
+                
+
+    #triggered_stories = [(s if (j.evaluate(s) is True for j in triggerlist) == True else None) for s in stories]
+    #print (triggered_stories)
+    return triggered_stories
+
+    
 
 
 
@@ -147,7 +300,32 @@ def read_trigger_config(filename):
     # line is the list of lines that you need to parse and for which you need
     # to build triggers
 
+    trigger_list = []
+    comp_dict = {}
+
+    for i in range(len(lines)):
+        line = lines[i].split(',')
+        if line[1] == 'TITLE':
+            comp_dict[line[0]] = TitleTrigger(line[2])
+        if line[1] == 'DESCRIPTION':
+            comp_dict[line[0]] = DescriptionTrigger(line[2])
+        if line[1] == 'BEFORE':
+            comp_dict[line[0]] = BeforeTrigger(line[2])
+        if line[1] == 'AFTER':
+            comp_dict[line[0]] = AfterTrigger(line[2])
+        if line[1] == 'AND':
+            comp_dict[line[0]] = AndTrigger(line[2], line[3])
+        if line[0] == 'ADD':
+            for j in range(1, len(line)):
+                trigger_list.append(comp_dict.get(line[j]))
+        
+    return trigger_list
+
+
+
     print(lines) # for now, print it so you see what it contains!
+
+read_trigger_config('triggers.txt')
 
 
 
